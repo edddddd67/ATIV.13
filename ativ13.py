9f2c4a365e903dce3ea750702f1251062e4d6c9f
edddddd67 <edjuniormendes74@email.com>
2024-09-11 16:04:00 -0300

    atividade 13

---
+++ ativ13.py
@@ -0,0 +1,11 @@
+# Crie um programa que receba um número do usuário e informe se ele é positivo, negativo ou zero.
+ed = float(input("numero do usuario"))
+if (ed <0):
+    print("Negativo")
+
+elif (ed == 0):
+    print("neutro")
+
+elif (ed >0):
+    print("positivo")
+    
